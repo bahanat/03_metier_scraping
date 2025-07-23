d67aacfdbc665b906118dde49646a444d74ea773
bahanat <bahurel.anatole@gmail.com>
2025-07-23 15:24:06 +0200

    feat: :construction: Ajout d'un fichier utils et normalisations spécifiques au Spider des produits Venessens
    Fin du développement d'une première version du Spider des produits depuis les pages "mosaïques" et création d'un fichier utils.py pour contenir les fonctions de normalisation à effectuer directement dans le Spider à priori.

--- bricoscraper/bricoscraper/spiders/produitspider.py
+++ bricoscraper/bricoscraper/spiders/produitspider.py
@@ -1,26 +1,79 @@
 import scrapy
 
+from ..utils import (
+    extract_devise,
+    extract_float,
+    extract_int,
+    remove_substring,
+    convert_str_to_bool,
+)
+
 
 class ProduitSpider(scrapy.Spider):
     name = "produitspider"
     allowed_domains = ["venessens-parquet.com"]
     start_urls = [
-        "https://venessens-parquet.com/collection/les-parquets-dinterieur/parquet-semi-massif/"
+        "https://venessens-parquet.com/collection/les-parquets-dinterieur/parquets-motifs/dalles-de-versailles/"
     ]
 
     def parse(self, response):
         produits = response.css("ul.products li.product")
 
         for produit in produits:
-
-            yield {
-                "label": produit.css("h2::text").get(),
-                "prix_ht": produit.css("span.price bdi::text").get(),
-                "prix_ttc": produit.css("span.priceRight bdi::text").get(),
-                "url": produit.css('a::attr("href")').get(),
-            }
+            produit_url = produit.css('a::attr("href")').get()
+            yield response.follow(produit_url, callback=self.parse_produit)
 
         page_suivante = response.css("a.next::attr(href)").get()
 
         if page_suivante is not None:
             yield response.follow(page_suivante, callback=self.parse)
+
+    def parse_produit(self, response):
+        url = response.url
+
+        # Extraction des données de la table des détails en un dictionnaire
+        details = {
+            th.get(): td.get()
+            for th, td in zip(
+                response.xpath('//div[@class="accordionContent"]//th/text()'),
+                response.xpath('//div[@class="accordionContent"]//td/text()'),
+            )
+        }
+
+        yield {
+            "url": url,
+            "label": details.get("nom du produit"),
+            "id": url.rstrip("/").split("/")[-1],
+            "ref_interne": response.css("span.reference::text").get(),
+            "categorie": response.xpath(
+                '//nav[@class="woocommerce-breadcrumb"]/a[3]/text()'
+            ).get(),
+            "url_image": response.css(
+                'div.woocommerce-product-gallery__image a::attr("href")'
+            ).get(),
+            "prix_ht": extract_float(response.css("span.prix::text").get()),
+            "prix_ttc": extract_float(response.css("span.tva::text").get()),
+            "devise": extract_devise(response.css("span.prix::text").get()),
+            "type_prix": response.css("span.prix::text").get().split("/")[-1],
+            "description": response.css(
+                "div.elementor-widget-woocommerce-product-content p::text"
+            ).get(),
+            "disponibilite": remove_substring(
+                response.css("span.disponibilite::text").get(), "Disponibilité "
+            ),
+            "origine": details.get("fabrication"),
+            "normes": details.get("normes"),
+            "compatibilite_cas": convert_str_to_bool(
+                details.get("compatible sol chauffant")
+            ),
+            "teinte": details.get("teinte"),
+            "essence": details.get("essence de bois"),
+            "caractere": details.get("caractere"),
+            "finition": details.get("finition"),
+            "epaisseur_mm": extract_int(details.get("epaisseur")),
+            "longueur_mm": extract_int(details.get("longueur")),
+            "largeur_mm": extract_int(details.get("largeur")),
+            "couche_usure_mm": extract_int(details.get("couche dusure")),
+            "type_lame": details.get("type de lame"),
+            "chanfrein": details.get("chanfrein"),
+        }
---
+++ bricoscraper/bricoscraper/utils.py
@@ -0,0 +1,39 @@
+import re
+
+
+def extract_devise(price_str):
+    if not price_str:
+        return None
+    match = re.search(r"([€$£])", price_str)
+    return match.group(1) if match else None
+
+
+def extract_float(str):
+    if not str:
+        return None
+    match = re.search(r"[\d.,]+", str)
+    return float(match.group().replace(",", ".")) if match else None
+
+
+def extract_int(str):
+    if not str:
+        return None
+    match = re.search(r"[\d.,]+", str)
+    return int(match.group().replace(",", ".")) if match else None
+
+
+def remove_substring(full_string, to_remove):
+    if not full_string or not to_remove:
+        return full_string
+    return full_string.replace(to_remove, "")
+
+
+def convert_str_to_bool(str):
+    if str is None:
+        return None
+    str = str.strip().lower()
+    if str == "oui":
+        return True
+    if str == "non":
+        return False
+    return None
