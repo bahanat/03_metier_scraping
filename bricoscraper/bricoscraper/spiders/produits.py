b8c8e32d6f014bb9f489375de64627309728a669
bahanat <bahurel.anatole@gmail.com>
2025-07-24 11:45:00 +0200

    refactor: :truck: Fichier renommé pour le spider des produits pour garder une cohérence avec le spider des catégories

--- bricoscraper/bricoscraper/spiders/produits.py
+++ bricoscraper/bricoscraper/spiders/produits.py
@@ -1,17 +1,17 @@
 import scrapy
 
 from ..utils import (
-    extract_devise,
-    extract_float,
-    extract_int,
-    remove_substring,
-    convert_str_to_bool,
-    number_in_range,
+    extraire_devise,
+    extraire_float,
+    extraire_int,
+    supprimer_substring,
+    convert_str_en_bool,
+    nombre_compris_entre,
 )
 
 
-class ProduitSpider(scrapy.Spider):
-    name = "produitspider"
+class ProduitsSpider(scrapy.Spider):
+    name = "produits"
     allowed_domains = ["venessens-parquet.com"]
     start_urls = [
         "https://venessens-parquet.com/collection/les-parquets-dinterieur/parquet-massif/"
@@ -30,7 +30,10 @@ class ProduitSpider(scrapy.Spider):
             yield response.follow(page_suivante, callback=self.parse)
 
     def parse_produit(self, response):
-        url = response.url
+        url_produit = response.url
+        url_categories = response.xpath(
+            '//nav[@class="woocommerce-breadcrumb"]/a/@href'
+        ).getall()
 
         # Extraction des données de la table des détails en un dictionnaire
         details = {
@@ -42,44 +45,44 @@ class ProduitSpider(scrapy.Spider):
         }
 
         yield {
-            "url": url,
+            "url": url_produit,
             "label": details.get("nom du produit"),
-            "id": url.rstrip("/").split("/")[-1],
-            "ref_interne": extract_int(response.css("span.reference::text").get()),
-            "categorie": response.xpath(
-                '//nav[@class="woocommerce-breadcrumb"]/a[3]/text()'
-            ).get(),
+            "id_produit": url_produit.rstrip("/").split("/")[-1],
+            "ref_interne": supprimer_substring(
+                response.css("span.reference::text").get(), "Ref: "
+            ),
+            "id_categorie": url_categories[-1].rstrip("/").split("/")[-1],
             "url_image": response.css(
                 'div.woocommerce-product-gallery__image a::attr("href")'
             ).get(),
-            "prix_ht": extract_float(response.css("span.prix::text").get()),
-            "prix_ttc": extract_float(response.css("span.tva::text").get()),
-            "devise": extract_devise(response.css("span.prix::text").get()),
+            "prix_ht": extraire_float(response.css("span.prix::text").get()),
+            "prix_ttc": extraire_float(response.css("span.tva::text").get()),
+            "devise": extraire_devise(response.css("span.prix::text").get()),
             "type_prix": response.css("span.prix::text").get().split("/")[-1],
             "description": response.css(
                 "div.elementor-widget-woocommerce-product-content p::text"
             ).get(),
-            "disponibilite": remove_substring(
+            "disponibilite": supprimer_substring(
                 response.css("span.disponibilite::text").get(), "Disponibilité "
             ),
             "origine": details.get("fabrication"),
             "normes": details.get("normes"),
-            "compatibilite_cas": convert_str_to_bool(
+            "compatibilite_cas": convert_str_en_bool(
                 details.get("compatible sol chauffant")
             ),
             "teinte": details.get("teinte"),
             "essence": details.get("essence de bois"),
             "caractere": details.get("caractere"),
             "finition": details.get("finition"),
-            "epaisseur_mm": number_in_range(
-                extract_int(details.get("epaisseur")), 0, 100
-            ),  # On renvoie None si l'épaisseur n'est pas comprise entre 0 et 100mm
-            "largeur_mm": number_in_range(
-                extract_int(details.get("largeur")), 30
-            ),  # On renvoie None si la largeur n'est pas supérieure à 30mm
-            "couche_usure_mm": number_in_range(
-                extract_int(details.get("couche dusure")), 0, 30
-            ),  # On renvoie None si la couche d'usure est supérieur à 30mm
+            "epaisseur_mm": nombre_compris_entre(
+                extraire_int(details.get("epaisseur")), 0, 100
+            ),  # On renvoie -1 si l'épaisseur n'est pas comprise entre 0 et 100mm
+            "largeur_mm": nombre_compris_entre(
+                extraire_int(details.get("largeur")), 30
+            ),  # On renvoie -1 si la largeur n'est pas supérieure à 30mm
+            "couche_usure_mm": nombre_compris_entre(
+                extraire_int(details.get("couche dusure")), 0, 30
+            ),  # On renvoie -1 si la couche d'usure est supérieur à 30mm
             "type_lame": details.get("type de lame"),
             "chanfrein": details.get("chanfrein"),
         }
