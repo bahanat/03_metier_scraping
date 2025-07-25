c5c6a5dcb991cfe550954846c144978e7c1ebd72
Cesar Gattano <cesar.gattano@gmail.com>
2025-07-25 10:57:20 +0200

    feat: :sparkles: Ajout des pipelines BricoscraperCleaningPipeline et BricoScraperTransformPipeline
    Développement et modification des settings pour les appeler

--- bricoscraper/bricoscraper/pipelines.py
+++ bricoscraper/bricoscraper/pipelines.py
@@ -8,7 +8,7 @@
 from itemadapter import ItemAdapter
 from scrapy import Spider
 from scrapy.exceptions import DropItem
-from .items import CategorieItem
+from .items import CategorieItem, ProduitItem
 
 
 class BricoscraperPipeline:
@@ -35,22 +35,67 @@ class BricoscraperPipeline:
 
         return item
 
+
 class BricoscraperCleaningPipeline:
 
     def process_item(self, item, spider: Spider):
 
-        adapter = ItemAdapter(item)
+        # Traitement des produits
+        if isinstance(item, ProduitItem):
+            id_produit = item["id"]
+            if isinstance(id_produit, str):
+                id_produit = id_produit.strip().lower()
+                if id_produit.startswith("nom-du-produit"):
+                    raise DropItem(f"Produit aberrant nettoyé: id={id_produit}")
 
-        ## switch some fields to lowercase
-        champs_minuscules = ["id", "id_parent", "ref_interne", "id_categorie", "type_prix"]
+            else:
+                raise DropItem(f"Produit nettoyé: non-string id={id_produit}")
+
+        return item
+
+
+class BricoScraperTransformPipeline:
+
+    def process_item(self, item, spider: Spider):
+
+        adapter = ItemAdapter(item)
+        self.mise_en_minuscule(adapter)
+        self.type_de_prix_non_vide(adapter)
+
+        return item
+
+    def mise_en_minuscule(self, adapter: ItemAdapter):
+        """Mise en minuscules de certains champs de données:
+        - id
+        - id_parent
+        - id_categorie
+        - ref_interne
+        - type_prix
+
+        Args:
+            adapter (ItemAdapter): L'item passant à travers le pipeline
+        """
+
+        champs_minuscules = [
+            "id",
+            "id_parent",
+            "id_categorie",
+            "ref_interne",
+            "type_prix",
+        ]
         for champ_minuscule in champs_minuscules:
             valeur = adapter.get(champ_minuscule)
             if isinstance(valeur, str):
                 adapter[champ_minuscule] = valeur.lower()
 
-        ## pas de chaine vide pour le type de prix, u par défaut
+    def type_de_prix_non_vide(self, adapter: ItemAdapter):
+        """Assure que le champ type_prix ne soit pas vide.
+        Par défaut ce champ doit contenir 'u' pour prix à l'unité
+
+        Args:
+            adapter (ItemAdapter): L'item passant à travers le pipeline
+        """
+
         valeur = adapter.get("type_prix")
         if isinstance(valeur, str) and not valeur:
             adapter["type_prix"] = "u"
-
-        return item
